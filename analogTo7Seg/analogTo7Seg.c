74f636d3a90d2b61c58e71441214cf0e5cb6b94d
Ethan Durrant <emd519@yahoo.com>
2014-05-27 19:28:49 -0700

    fixed ghosting in some segments, seems stable

--- analogTo7Seg/analogTo7Seg.c
+++ analogTo7Seg/analogTo7Seg.c
@@ -14,6 +14,7 @@
 #define MOSI BIT6 // SER IN or Data on shift register
 #define SCLK BIT5 // SRCK or Clock on shift register
 
+
 //prototypes
 void clear_display(void);
 void write_digit(unsigned char num, unsigned char dig);
@@ -27,6 +28,8 @@ void msg_error(void);
 
 //add any defined digits to this array
 unsigned char digit_bits[] = { DIG_0, DIG_1, DIG_2, DIG_3 };
+unsigned char num_digits = sizeof(digit_bits);
+
 //int i used for "for" loops
 int i;
 
@@ -61,10 +64,6 @@ int main(void)
 	{
 		//msg_error();
 		write_number(2345);
-		//__delay_cycles(1000);
-		//P2OUT |= ALL_DIGS;
-		//bb_shift_out(0b01010101);
-		//flip_latch();
 	}
 	return 0; //should never reach this point
 }
@@ -88,9 +87,6 @@ void bb_shift_out(unsigned char data){
 		P1OUT &= ~(SCLK); //LOW
 		//__delay_cycles(100);
 		P1OUT |= SCLK; //HIGH
-		//for(i = 0; i < 10; i++){
-		//__delay_cycles(100);
-		//}
 	}
 }
 
@@ -102,8 +98,8 @@ void write_digit(unsigned char num, unsigned char dig){
 	if(num < 10){
 	bb_shift_out(number_seg_bytes[num]);
 	} else {bb_shift_out(number_seg_bytes[10]);}
-	flip_latch();
-	for( k = 0; k < sizeof(digit_bits); k++){
+	//flip_latch();
+	for( k = 0; k < num_digits; k++){
 		if ( k == dig ){
 			p2stuff |= digit_bits[k];
 		} else {
@@ -111,7 +107,7 @@ void write_digit(unsigned char num, unsigned char dig){
 		}
 	}
 	P2OUT = p2stuff;
-	//flip_latch();
+	flip_latch();
 	//__delay_cycles(5000);
 }
 
@@ -125,13 +121,12 @@ void write_number(unsigned int number){
 		//check if number is too big ot not
 		if (number < 10000){
 			//formats number based on digits to correct digits on display
-			for(h=0;h<4;h++){
+			for(h=0;h < num_digits;h++){
 				write_digit(format_num % 10, h);
 				format_num /= 10;
 			}         
 		} else {
 			msg_error();
 		}
-		//__delay_cylcles(1000);
 }
 
